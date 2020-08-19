1069021db6ae1f83a39099c8bdb23c4135cb42bb
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-08-20 01:26:42 +0530

    adding audio

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 document.addEventListener("keypress", function(e) {
     if(e.code == "KeyA") {
-        console.log("The 'A' key is pressed.");
+        let audA = new Audio("audio/A.mp3");
+        audA.play();
+        // console.log("The 'A' key is pressed.");
     }
     else if(e.code == "KeyS") {
-        console.log("The 'S' key is pressed.");
+        let audS = new Audio("audio/S.mp3");
+        audS.play();
+        // console.log("The 'S' key is pressed.");
     }
     else if(e.code == "KeyD") {
-        console.log("The 'D' key is pressed.");
+        let audD = new Audio("audio/D.mp3");
+        audD.play();
+        // console.log("The 'D' key is pressed.");
     }
     else if(e.code == "KeyF") {
-        console.log("The 'F' key is pressed.");
+        let audF = new Audio("audio/F.mp3");
+        audF.play();
+        // console.log("The 'F' key is pressed.");
     }
     else if(e.code == "KeyG") {
-        console.log("The 'G' key is pressed.");
+        let audG = new Audio("audio/G.mp3");
+        audG.play();
+        // console.log("The 'G' key is pressed.");
     }
     else if(e.code == "KeyH") {
-        console.log("The 'H' key is pressed.");
+        let audH = new Audio("audio/H.mp3");
+        audH.play();
+        // console.log("The 'H' key is pressed.");
     }
     else if(e.code == "KeyJ") {
-        console.log("The 'J' key is pressed.");
+        let audJ = new Audio("audio/J.mp3");
+        audJ.play();
+        // console.log("The 'J' key is pressed.");
     }
     else {
         console.log("Some other key was pressed.")
